874b466378dbbe946379e2ba9a0f1ba3a400f97a
ayoshi <89048791+ayoshi007@users.noreply.github.com>
2024-03-29 23:53:08 -0500

    Edit tab indentation and space settings

--- init.lua
+++ init.lua
@@ -860,3 +860,9 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+-- !!! MY CONFIGURAIONS START HERE
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 0
+vim.opt.smarttab = true
